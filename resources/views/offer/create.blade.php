0e0e3916f2a015c4e24e35778994c83e9b0c5713
Patryk Pawłowicz <patryk.pawlowicz@wp.pl>
2020-12-29 20:03:02 +0100

    ok

--- resources/views/offer/create.blade.php
+++ resources/views/offer/create.blade.php
@@ -16,14 +16,14 @@
   <div class="form-group">
     <label for="text" class="col-sm-2 control-label">Localization</label>
     <div class="col-sm-10">
-      <input type="text" class="form-control" id="localization" placeholder="Enter the offer name">
+      <input type="text" class="form-control" id="localization" placeholder="Enter the place localization">
     </div>
   </div>
 
   <div class="form-group">
     <label for="text" class="col-sm-2 control-label">Price</label>
     <div class="col-sm-10">
-      <input type="text" class="form-control" id="price" placeholder="Enter the offer name">
+      <input type="text" class="form-control" id="price" placeholder="Enter the offer price">
     </div>
   </div>
 
@@ -33,15 +33,15 @@
       <input type="text" class="form-control" id="description" placeholder="Enter the offer description">
     </div>
   </div>
-  <!-- <div class="form-group">
-    <div class="col-sm-offset-2 col-sm-10">
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Zaznacz mnie
-        </label>
-      </div>
+  
+  <div class="form-group">
+    <label for="addphoto" class="col-sm-2 control-label" >Add place photos</label>
+
+    <div class="col-sm-10">
+    <input type="file" class="form-control-file" id="addphoto" data-show-upload="false" data-show-caption="true" multiple>
     </div>
-  </div> -->
+  </div>
+
   <div class="form-group">
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-dark">Create</button>
